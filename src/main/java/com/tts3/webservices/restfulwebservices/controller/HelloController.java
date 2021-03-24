8d701875e2f486fe8461f47b3b3c7e54e2e31a2f
Jake Wilson <jwilsonlax13@gmail.com>
2021-03-23 21:06:28 -0400

    added Internationalization functionality for restful webservices

--- src/main/java/com/tts3/webservices/restfulwebservices/controller/HelloController.java
+++ src/main/java/com/tts3/webservices/restfulwebservices/controller/HelloController.java
@@ -2,12 +2,21 @@ package com.tts3.webservices.restfulwebservices.controller;
 
 
 import com.tts3.webservices.restfulwebservices.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    MessageSource messageSource;
 
     //spring boot autoconfiguration dispatcher servlet is handling
     //all of the requests
@@ -29,5 +38,10 @@ public class HelloController {
     @GetMapping(value = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(value = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
